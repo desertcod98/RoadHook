bb275685ae9d59f8487a8ae18fbf3a4ca0e0aa61
des98 <dev.desertcod98@proton.me>
2024-09-17 16:08:51 +0200

    Initial commit

---
+++ RoadHook.c
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "framework.h"
+#include <stdio.h>
+#include "include/RoadHook.h"
+
+// TODO: This is an example of a library function
+void fnRoadHook()
+{
+	printf("testasdasd");
+}
